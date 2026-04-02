c46328cd104908b621fb28c1ce5b6c6d4432ce12
Harika <harikahari034.gmail.com>
2026-04-02 16:25:56 +0530

    hapy

--- cookies.py
+++ cookies.py
@@ -5,7 +5,7 @@ import time
 driver=webdriver.Chrome()
 
 driver.get("https://demo.nopcommerce.com/")
-time.sleep(15)
+time.sleep(5)
 driver.maximize_window()
 
 
@@ -16,7 +16,7 @@ print(len(cookies))
 for cookie in cookies:
     print(cookie)
     print(cookie.get('name'),':',cookie.get('value'))
-
+#secind try for commit message
 driver.delete_cookie("Harika")
 driver.delete_all_cookies()
 driver.get_cookies()
