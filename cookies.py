779305b06f0e3ce89ecccbc54229d9fa815960ca
Harika <harikahari034.gmail.com>
2026-04-02 16:22:34 +0530

    hPPY NEW YR

--- cookies.py
+++ cookies.py
@@ -5,12 +5,14 @@ import time
 driver=webdriver.Chrome()
 
 driver.get("https://demo.nopcommerce.com/")
+time.sleep(15)
 driver.maximize_window()
+
 
 driver.add_cookie({"name":"Harika","value":"2010"})
 cookies = driver.get_cookies()
 print(len(cookies))
-
+# hP=happy new year
 for cookie in cookies:
     print(cookie)
     print(cookie.get('name'),':',cookie.get('value'))
